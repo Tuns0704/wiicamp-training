6900c7c92f4592af633a2e2cae0a1e3508ffef80
Tuns0704 <tuanhm0704@gmail.com>
2024-06-26 22:37:56 +0700

    Update: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@/*': path.resolve(__dirname, './src'),
-      '@/components/*': path.resolve(__dirname, './src/components'),
-    },
-  },
+  base: '/',
 });
